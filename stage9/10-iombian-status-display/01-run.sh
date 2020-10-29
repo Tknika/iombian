1e08a88ee8ecc5fac7d6d3935bcc7dfd991d50d8
Aitor Iturrioz <riturrioz@gmail.com>
2020-10-29 17:36:37 +0100

    Fix iombian-status-display installation file
    Signed-off-by: Aitor Iturrioz <riturrioz@gmail.com>

---
+++ stage9/10-iombian-status-display/01-run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -e
+
+INSTALLATION_PATH="/opt/iombian-status-display"
+
+# Enable I2C and SPI
+on_chroot << EOF
+sed -i '/#dtparam=spi=on/c\dtparam=spi=on' /boot/config.txt
+sed -i '/#dtparam=spi=off/c\dtparam=spi=on' /boot/config.txt
+sed -i '/dtparam=spi=off/c\dtparam=spi=on' /boot/config.txt
+sed -i '/#dtparam=i2c_arm=on/c\dtparam=i2c_arm=on' /boot/config.txt
+sed -i '/#dtparam=i2c_arm=off/c\dtparam=i2c_arm=on' /boot/config.txt
+sed -i '/dtparam=i2c_arm=off/c\dtparam=i2c_arm=on' /boot/config.txt
+EOF
+
+# Create the installation folder
+on_chroot << EOF
+if [ -d "$INSTALLATION_PATH" ]; then
+  rm -rf ${INSTALLATION_PATH}
+fi
+mkdir -p $INSTALLATION_PATH
+EOF
+
+install -m 644 -g root -o root iombian-status-display/requirements.txt ${ROOTFS_DIR}$INSTALLATION_PATH
+install -m 644 -g root -o root iombian-status-display/src/* ${ROOTFS_DIR}$INSTALLATION_PATH
+install -m 644 -g root -o root iombian-status-display/systemd/iombian-status-display.service ${ROOTFS_DIR}$INSTALLATION_PATH
+
+# Create the virtual environment
+on_chroot << EOF
+chown -R $FIRST_USER_NAME:$FIRST_USER_NAME $INSTALLATION_PATH
+
+cd $INSTALLATION_PATH
+python3 -m venv venv
+source venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+EOF
