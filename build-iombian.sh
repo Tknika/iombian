87117064d913da863151e9c4dcdc846e659aac34
Aitor Iturrioz <aiturrioz@tknika.eus>
2022-01-19 12:28:49 +0100

    Do not make the build process fail silently
    Signed-off-by: Aitor Iturrioz <aiturrioz@tknika.eus>

--- build-iombian.sh
+++ build-iombian.sh
@@ -11,7 +11,7 @@ mv stage2/EXPORT_IMAGE stage2/.EXPORT_IMAGE
 mv stage2/EXPORT_NOOBS stage2/.EXPORT_NOOBS
 
 echo "Building iombian image ..."
-./build-docker.sh || true
+./build-docker.sh
 
 echo "Restoring stage2"
 mv stage2/.EXPORT_IMAGE stage2/EXPORT_IMAGE
