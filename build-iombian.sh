9286b706d9f705806a29af1b7418c9cd3a1273f7
Aitor Iturrioz <aiturrioz@tknika.eus>
2022-01-19 11:26:16 +0100

    Improve build process
    Signed-off-by: Aitor Iturrioz <aiturrioz@tknika.eus>

--- build-iombian.sh
+++ build-iombian.sh
@@ -1,13 +1,17 @@
 #!/bin/bash -e
 
-./update-submodules.sh
+./init-submodules.sh
+
+# Temporal fix until https://github.com/RPi-Distro/pi-gen/issues/561 is fixed
+echo "Fix bullseye issue"
+./fix-bullseye.sh
 
 echo "Modifying stage2"
 mv stage2/EXPORT_IMAGE stage2/.EXPORT_IMAGE
 mv stage2/EXPORT_NOOBS stage2/.EXPORT_NOOBS
 
 echo "Building iombian image ..."
-./build-docker.sh
+./build-docker.sh || true
 
 echo "Restoring stage2"
 mv stage2/.EXPORT_IMAGE stage2/EXPORT_IMAGE
