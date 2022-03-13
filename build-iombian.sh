69dd81811c6bb9c2d75b196380299cb5e4295273
Aitor Iturrioz <aiturrioz@tknika.eus>
2022-03-14 00:49:42 +0100

    Build with bullseye
    Signed-off-by: Aitor Iturrioz <aiturrioz@tknika.eus>

--- build-iombian.sh
+++ build-iombian.sh
@@ -6,10 +6,6 @@ echo "Modifying stage2"
 mv stage2/EXPORT_IMAGE stage2/.EXPORT_IMAGE
 mv stage2/EXPORT_NOOBS stage2/.EXPORT_NOOBS
 
-echo "Modifying stage2"
-mv stage2/EXPORT_IMAGE stage2/.EXPORT_IMAGE
-mv stage2/EXPORT_NOOBS stage2/.EXPORT_NOOBS
-
 echo "Building iombian image ..."
 ./build-docker.sh
 
