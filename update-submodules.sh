6480bb56f624a31203821ff82fb90a5fc1adfdde
Aitor Iturrioz <aiturrioz@tknika.eus>
2022-03-24 14:14:09 +0100

    Add IoMBian Tunnels Handler service (#17)

--- update-submodules.sh
+++ update-submodules.sh
@@ -9,6 +9,8 @@ git -C stage9/12-iombian-system-info-provider/iombian-system-info-provider pull 
 git -C stage9/13-iombian-display-handler/iombian-display-handler pull origin master
 git -C stage9/14-iombian-bluetooth-configurator/iombian-bluetooth-configurator pull origin master
 git -C stage9/15-iombian-remote-configurator/iombian-remote-configurator pull origin master
+git -c stage9/16-iombian-serial-configurator/iombian-serial-configurator pull origin master
+git -c stage9/19-iombian-tunnels-handler/iombian-tunnels-handler pull origin master
 git -C stage9/99-confinit/config pull origin master
 
 echo "Done."
