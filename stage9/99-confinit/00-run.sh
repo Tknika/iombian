acbb6a962b36660379e31bf8c029e723aeabdfbf
Aitor Iturrioz <aiturrioz@tknika.eus>
2023-05-18 17:06:39 +0200

    Add confinit intermediate script execution service
    Signed-off-by: Aitor Iturrioz <aiturrioz@tknika.eus>

--- stage9/99-confinit/00-run.sh
+++ stage9/99-confinit/00-run.sh
@@ -21,13 +21,16 @@ EOF
 
 # systemd units
 install -m 644 -g root -o root confinit/systemd/confinit-boot@.service ${ROOTFS_DIR}/lib/systemd/system
+install -m 644 -g root -o root confinit/systemd/confinit-intermediate@.service ${ROOTFS_DIR}/lib/systemd/system
 install -m 644 -g root -o root confinit/systemd/confinit-final@.service ${ROOTFS_DIR}/lib/systemd/system
 
 on_chroot <<EOF
 # Enable service
 mv "/lib/systemd/system/confinit-boot@.service" "/lib/systemd/system/confinit-boot@boot-config.service"
+mv "/lib/systemd/system/confinit-intermediate@.service" "/lib/systemd/system/confinit-intermediate@boot-config.service"
 mv "/lib/systemd/system/confinit-final@.service" "/lib/systemd/system/confinit-final@boot-config.service"
 systemctl enable "confinit-boot@boot-config.service"
+systemctl enable "confinit-intermediate@boot-config.service"
 systemctl enable "confinit-final@boot-config.service"
 mkdir -p /${BOOT_CONFIG_FOLDER}
 EOF
