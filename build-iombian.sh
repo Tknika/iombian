54a48de9e8c11aace57e6efec3e0342d57bb935b
Aitor Iturrioz <aiturrioz@tknika.eus>
2023-05-26 21:56:35 +0200

    Define image generation stages manually (#29)

--- build-iombian.sh
+++ build-iombian.sh
@@ -6,13 +6,5 @@
 echo "Fix bullseye issue"
 ./fix-bullseye.sh
 
-echo "Modifying stage2"
-mv stage2/EXPORT_IMAGE stage2/.EXPORT_IMAGE
-mv stage2/EXPORT_NOOBS stage2/.EXPORT_NOOBS
-
 echo "Building iombian image ..."
 ./build-docker.sh
-
-echo "Restoring stage2"
-mv stage2/.EXPORT_IMAGE stage2/EXPORT_IMAGE
-mv stage2/.EXPORT_NOOBS stage2/EXPORT_NOOBS
