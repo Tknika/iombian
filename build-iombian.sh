07931c3ac980be9be5e5817d15e6e74ce8d06cec
Aitor Iturrioz <aiturrioz@tknika.eus>
2023-10-24 11:04:08 +0200

    Build with bullseye (#36)
    Signed-off-by: Aitor Iturrioz <aiturrioz@tknika.eus>

--- build-iombian.sh
+++ build-iombian.sh
@@ -2,10 +2,6 @@
 
 ./init-submodules.sh
 
-# Temporal fix until https://github.com/RPi-Distro/pi-gen/issues/561 is fixed
-echo "Fix bullseye issue"
-./fix-bullseye.sh
-
 echo "Modifying stage2"
 mv stage2/EXPORT_IMAGE stage2/.EXPORT_IMAGE
 mv stage2/EXPORT_NOOBS stage2/.EXPORT_NOOBS
--- fix-bullseye.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-# GitHub issue: https://github.com/RPi-Distro/pi-gen/issues/561 
-echo "Compile using buster instead of bullseye ..."
-sed -i 's/:bullseye/:buster/' ./build-docker.sh
-sed -i 's/:bullseye/:buster/' ./Dockerfile
-
-echo "Done."
