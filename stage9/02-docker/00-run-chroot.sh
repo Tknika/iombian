4736ae27af8f861d7edb6ab218dfac2cb32d0950
Aitor Iturrioz <aiturrioz@tknika.eus>
2024-05-13 20:18:07 +0200

    Remove installed packages check for Docker installation
    Signed-off-by: Aitor Iturrioz <aiturrioz@tknika.eus>

--- stage9/02-docker/00-run-chroot.sh
+++ stage9/02-docker/00-run-chroot.sh
@@ -1,13 +1,7 @@
 #!/bin/bash -e
 
 # Uninstall all conflicting packages
-for pkg in docker.io docker-doc docker-compose containerd runc; 
-do 
-  $(dpkg --status $pkg &> /dev/null)
-  if [[ $? -eq 0 ]]; then
-    apt-get remove $pkg;
-  fi
-done
+for pkg in docker.io docker-doc docker-compose containerd runc; do apt-get remove $pkg; done
 
 # Set up Docker's apt repository
 apt-get update
