7fadfde5b66ed25ec9d623dd1d53d2b2b01a6d54
Aitor Iturrioz <aiturrioz@tknika.eus>
2024-05-13 22:28:53 +0200

    Temporally disable docker network creation
    Signed-off-by: Aitor Iturrioz <aiturrioz@tknika.eus>

--- stage9/03-docker-networks/00-run-chroot.sh
+++ stage9/03-docker-networks/00-run-chroot.sh
@@ -4,7 +4,7 @@ DOCKER_INTERNAL_NETWORK_NAME=iombian-internal-services
 DOCKER_EXTERNAL_NETWORK_NAME=iombian-external-services
 
 # Install internal network
-docker network create ${DOCKER_INTERNAL_NETWORK_NAME}
+#docker network create ${DOCKER_INTERNAL_NETWORK_NAME}
 
 # Install external network
-docker network create ${DOCKER_EXTERNAL_NETWORK_NAME}
+#docker network create ${DOCKER_EXTERNAL_NETWORK_NAME}
